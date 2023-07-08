8838b37e258b82ca25ae0209cf61ece2b5db20f4
Kanny <kanny.ghafour87@gmail.com>
2023-07-08 13:50:01 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';  // Declare the variable in global scope
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase(); 
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';  
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; 
+}
+
+const leastFavoriteCustomer = 'John';  
+
+function changeLeastFavoriteCustomer() {
+  
+  leastFavoriteCustomer = 'Jane';  
+}
